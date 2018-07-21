14caff9655b23ac5681c17298545a9a104178d4f
kainmaligno <s.e.jugr@gmail.com>
2018-07-21 16:49:02 -0500

    base en la nube de seed

--- bin/seedCatalogo.js
+++ bin/seedCatalogo.js
@@ -3,7 +3,7 @@ const Modulo = require('../models/modulo');
 // const Tema = require('../models/tema');
 // const Postit = require('..models/postit');
 
-mongoose.connect('mongodb://localhost/RetrospectiveProject');
+mongoose.connect('mongodb://bliss:abc123@ds145981.mlab.com:45981/retrospective');
 
 const newModulo = Modulo({
     modulo : 'Modulo uno'
@@ -40,24 +40,6 @@ newModulo3.save()
 .catch((err)=>{
   console.log(err);
 });
-
-
-
-
-
-// const newTema =  Tema ();
-// newTema.insertMany([
-//   {tema: 'JavaScript'},{tema:'Html'},{tema:'Css'},{tema:'DOM'},{tema:'Bootstrap'},{tema:'Jquery'},{tema:'Nodejs'},{tema:'MongoDb'},{tema:'Express'},{tema:'Mongoose'},{tema:'Passport'},
-//   {tema:'API'},{tema:'Ajax'},{tema:'Json'},{tema:'Angular'},{tema:'Angular-Forms'},{tema:'Angular-Service'},{tema:'Angular-RestApi-Express'},{tema:'Angular-Recipes'},{tema:'Angular-Auth'}]);
-
-
-// newTema.save()
-// .then(()=>{
-//   console.log("nuevo tema agregado...");
-// })
-// .catch((err)=>{
-//   console.log(err);
-// });
 
 
 
--- bin/seedTemas.js
+++ bin/seedTemas.js
@@ -3,40 +3,37 @@ const mongoose = require('mongoose');
  const Tema = require('../models/tema');
 // const Postit = require('..models/postit');
 
-mongoose.connect('mongodb://localhost/RetrospectiveProject');
+mongoose.connect('mongodb://bliss:abc123@ds145981.mlab.com:45981/retrospective');
 
-const newTema = Tema({
-    tema : 'JavaScript',
-    moduloid: "5b53a1a58b92cd75cdf9b613"
-})
-
-newTema.save()
-.then(()=>{
-  console.log("Tema agregado...");
-})
-.catch((err)=>{
-  console.log(err);
-});
-
-const newTema2 = Tema({
-    tema : 'Html',
-    moduloid: "5b53a1a58b92cd75cdf9b613"
-})
-
-newTema2.save()
-.then(()=>{
-  console.log("Tema agregado2...");
-})
-.catch((err)=>{
-  console.log(err);
-});
+let modulo1 = [
+       {tema: 'JavaScript', moduloid:"5b53a1a58b92cd75cdf9b613"},
+       {tema:'Html', moduloid:"5b53a1a58b92cd75cdf9b613"},
+       {tema:'Css', moduloid:"5b53a1a58b92cd75cdf9b613"},
+       {tema:'DOM', moduloid:"5b53a1a58b92cd75cdf9b613"},
+       {tema:'Bootstrap', moduloid:"5b53a1a58b92cd75cdf9b613"},
+       {tema:'Jquery', moduloid:"5b53a1a58b92cd75cdf9b613"}];
+    Tema.insertMany(modulo1, function(error, docs) {});
 
 
+let modulo2 = [
+    {tema:'Nodejs', moduloid:"5b53a1a58b92cd75cdf9b614"},
+    {tema:'MongoDb',moduloid:"5b53a1a58b92cd75cdf9b614"},
+    {tema:'Express',moduloid:"5b53a1a58b92cd75cdf9b614"},
+    {tema:'Mongoose', moduloid:"5b53a1a58b92cd75cdf9b614"},
+    {tema:'Passport',moduloid:"5b53a1a58b92cd75cdf9b614"}];
+    Tema.insertMany(modulo2,function(error, docs){});
+
+let modulo3 = [
+    {tema:'API', moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Ajax',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Json',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular-Forms',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular-Service',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular-RestApi-Express',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular-Recipes',moduloid:"5b53a1a58b92cd75cdf9b615"},
+    {tema:'Angular-Auth',moduloid:"5b53a1a58b92cd75cdf9b615"}];
+    Tema.insertMany(modulo3,function(error,docs){});   
 
 
-
-
-// ['JavaScript','Html','Css','DOM','Bootstrap','Jquery',]
-// ['Nodejs','MongoDb','Express','Mongoose','Passport','API','Ajax','Json']
-// ['Angular','Angular-Forms','Angular-Service','Angular-RestApi-Express','Angular-Recipes','Angular-Auth']
     
